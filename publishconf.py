53f1857a5c7a2316b2ec94ad1f1fe7c6f6bf5958
Leonidas Liakos <leonidas.liakos@ext.ec.europa.eu>
2026-04-08 15:08:33 +0300

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://www.gislab.gr'
+SITEURL = 'https://www.gislab.gr'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
